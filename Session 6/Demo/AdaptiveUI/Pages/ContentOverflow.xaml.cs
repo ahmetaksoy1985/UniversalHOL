3dcea46cf73b5e26a2c4e699dfe6b0c04b28a25e
ardentm1 <ardentm@hotmail.com>
2014-06-24 23:17:52 +0900

    Session 6 추가

---
+++ Session 6/Demo/AdaptiveUI/Pages/ContentOverflow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace AdaptiveUI.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ContentOverflow : Page
+    {
+        public ContentOverflow()
+        {
+            this.InitializeComponent();
+            VisualStateManager.GoToState(this, "Portrait", false);
+            DisplayInformation displayInfo = DisplayInformation.GetForCurrentView();
+            displayInfo.OrientationChanged += DisplayInfoOrientationChanged;
+        }
+
+        private void DisplayInfoOrientationChanged(DisplayInformation sender, object args)
+        {
+            var orientation = sender.CurrentOrientation;
+            if (orientation == DisplayOrientations.Landscape || orientation == DisplayOrientations.LandscapeFlipped)
+            {
+                var res = VisualStateManager.GoToState(this, "Landscape", true);
+            }
+            if (orientation == DisplayOrientations.Portrait || orientation == DisplayOrientations.PortraitFlipped)
+            {
+                var res = VisualStateManager.GoToState(this, "Portrait", false);
+            }
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
---
+++ Session 6/Demo/AdaptiveUI/Pages/ResponsiveUI.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace AdaptiveUI.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ResponsiveUI : Page
+    {
+        public ResponsiveUI()
+        {
+            this.InitializeComponent();
+            Loaded += ResponsiveUiLoaded;
+        }
+
+        void ResponsiveUiLoaded(object sender, RoutedEventArgs e)
+        {
+            var numbers = new List<int>();
+            for (int i = 1; i < 255; i++)
+            {
+                numbers.Add(i);
+            }
+            NumberGrid.ItemsSource = numbers;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
